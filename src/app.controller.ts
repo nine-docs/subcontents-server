e4a9dc2536319b50b29960f077dcf7a02b0cbcaa
hyosoek <tennfin1@gmail.com>
2025-02-05 14:43:47 +0900

    action test2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('health')
-  helloWorld() {
+  helloWorld1() {
     return {
       success: true,
     };
